7de881dc9343e9985bc1cf37f99e2086714d7931
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-05-25 15:29:58 -0300

    Migrei para o AWS

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('hello');
 });
 
