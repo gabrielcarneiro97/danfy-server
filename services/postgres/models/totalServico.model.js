3e8384d0e7dce01b59cbddb044b677a740b65537
Gabriel Carneiro <gabriel.carneiro97@live.com>
2019-06-03 14:49:25 -0300

    TotalServico model pronto

---
+++ services/postgres/models/totalServico.model.js
@@ -0,0 +1,34 @@
+const Table = require('./table.model');
+
+class TotalServico extends Table {
+  constructor(obj, isSnake) {
+    super(obj, isSnake, TotalServico);
+  }
+
+  static tbName() {
+    return 'tb_total_servico';
+  }
+
+  static tbUK() {
+    return 'id';
+  }
+
+  static columns() {
+    return [
+      'id',
+      'total',
+      'imposto_id',
+      'retencao_id',
+    ];
+  }
+
+  static async getBy(column, value) {
+    return Table.getBy(column, value, TotalServico);
+  }
+
+  save() {
+    return Table.save(this, TotalServico);
+  }
+}
+
+module.exports = TotalServico;
