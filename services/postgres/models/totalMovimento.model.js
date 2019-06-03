f5cee6f65d3db9c8234b68b701cb45d3c1b0e3a5
Gabriel Carneiro <gabriel.carneiro97@live.com>
2019-06-03 14:48:12 -0300

    TotalMovimento model pronto

---
+++ services/postgres/models/totalMovimento.model.js
@@ -0,0 +1,34 @@
+const Table = require('./table.model');
+
+class TotalMovimento extends Table {
+  constructor(obj, isSnake) {
+    super(obj, isSnake, TotalMovimento);
+  }
+
+  static tbName() {
+    return 'tb_total_movimento';
+  }
+
+  static tbUK() {
+    return 'id';
+  }
+
+  static columns() {
+    return [
+      'id',
+      'imposto_id',
+      'valor_saida',
+      'lucro',
+    ];
+  }
+
+  static async getBy(column, value) {
+    return Table.getBy(column, value, TotalMovimento);
+  }
+
+  save() {
+    return Table.save(this, TotalMovimento);
+  }
+}
+
+module.exports = TotalMovimento;
