6a32a74f0f553908ff099ba943592dab13fe57d7
Gabriel Carneiro <gabriel.carneiro97@live.com>
2019-12-17 12:08:29 -0300

    tudo resolvido

--- routers/grupo.router.js
+++ routers/grupo.router.js
@@ -25,15 +25,49 @@ grupoRouter.post('/:cnpj', bodyParser.json(), async (req, res) => {
 
   const { cnpj } = req.params;
   try {
-    const grupo = new Grupo({
+    await new Grupo({
       donoCpfcnpj: cnpj,
       descricao,
       nome,
       cor,
-    });
-    await grupo.save();
+    }).save();
     res.sendStatus(201);
   } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+grupoRouter.put('/:cnpj', bodyParser.json(), async (req, res) => {
+  const {
+    id,
+    descricao,
+    nome,
+    cor,
+  } = req.body;
+
+  const { cnpj } = req.params;
+
+  try {
+    const [grupo] = await Grupo.getBy({ id, donoCpfcnpj: cnpj });
+
+    if (!grupo) {
+      await new Grupo({
+        donoCpfcnpj: cnpj,
+        descricao,
+        nome,
+        cor,
+      }).save();
+      res.sendStatus(201);
+    } else {
+      grupo.nome = nome || grupo.nome;
+      grupo.descricao = descricao || grupo.descricao;
+      grupo.cor = cor || grupo.cor;
+
+      await grupo.save();
+      res.sendStatus(201);
+    }
+  } catch (err) {
+    console.error(err);
     res.status(500).send(err);
   }
 });
