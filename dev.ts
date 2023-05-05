cd6cc876f1f5320fae442b4ed72de10d0abe2336
castro150 <rafaelcarneiroget@hotmail.com>
2023-05-05 19:34:30 +0000

    From remote machine

--- dev.ts
+++ dev.ts
@@ -11,15 +11,15 @@ app.options('*', cors());
 app.use(cors());
 app.use('/api', danfy);
 
-if (process.argv[2] === 'ssl') {
-  https.createServer(SSL, app).listen(8080, () => {
-    console.log('SSL server listening 8080 port');
-  });
-} else {
-  app.listen(8080, () => {
-    console.log('Example app listening at http://localhost:8080');
-  });
-}
+// if (process.argv[2] === 'ssl') {
+//   https.createServer(SSL, app).listen(8080, () => {
+//     console.log('SSL server listening 8080 port');
+//   });
+// } else {
+//   app.listen(8080, () => {
+//     console.log('Example app listening at http://localhost:8080');
+//   });
+// }
 
 module.exports.handler = serverless(app);
 // export const handler = serverless(app);
--- services/pg.service.ts
+++ services/pg.service.ts
@@ -2,20 +2,20 @@ import * as config from 'pg';
 import * as knex from 'knex';
 import * as fs from 'fs';
 
-const { user, password } = require('./private.json');
+// const { user, password } = require('./private.json');
 
 config.types.setTypeParser(1700, parseFloat);
 config.defaults.ssl = true;
 
-export const cert = fs.readFileSync(`${__dirname}/rds-ca-2019-root.pem`, 'utf8');
+// export const cert = fs.readFileSync(`${__dirname}/rds-ca-2019-root.pem`, 'utf8');
 
 export const pg = knex({
   client: 'pg',
   connection: {
     database: 'danfy',
     host: 'danfy.ctzvj9qzh3yk.us-east-2.rds.amazonaws.com',
-    user,
-    password,
+    user: 'postgres',
+    password: 'Ga190797*',
     ssl: false,
     // ssl: {
     //   root: cert,
